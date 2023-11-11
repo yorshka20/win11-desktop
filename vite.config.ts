23d17b5323215c877b5db257259d4b54e1bcc39a
yorshka <mail@yors.hk>
2023-11-11 14:20:23 +0900

    build: chunk output

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          'style-vendors': [
+            'styled-components',
+            '@emotion/styled',
+            '@emotion/react',
+            '@mui/joy',
+            '@mui/styled-engine-sc',
+            '@mui/icons-material',
+          ],
+          interacts: ['re-resizable', 'react-draggable'],
+          'common-vendors': ['lodash-es', 'moment'],
+        },
+      },
+    },
+  },
 });
